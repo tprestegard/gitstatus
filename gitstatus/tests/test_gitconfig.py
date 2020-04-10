55a53d494622b253ccba4478c5ee73f2d986f89f
Tanner Prestegard <tprestegard@gmail.com>
2020-04-10 09:54:12 -0500

    Finish config loading test

--- gitstatus/tests/test_gitconfig.py
+++ gitstatus/tests/test_gitconfig.py
@@ -44,7 +44,7 @@ def config():
 ###############################################################################
 # TESTS #######################################################################
 ###############################################################################
-def test_remove_config_comments(gitrepo_instance):
+def test_remove_config_comments():
     # Setup
     str_with_comments = textwrap.dedent("""
         ; ok test
@@ -79,6 +79,33 @@ def test_remove_config_comments(gitrepo_instance):
     
 
 def test_parse_gitconfig(config):
-    out = _parse_gitconfig(config)
-    import ipdb; ipdb.set_trace()
-    x = 1
+    expected_result = {
+        "core": {
+            "repositoryformatversion": "0",
+            "filemode": "true",
+            "bare": "false",
+            "logallrefupdates": "true",
+        },
+        "user": {
+            "email": "user@email.com",
+            "signingkey": "41391Q731V9VS020",
+        },
+        "branch": {
+            "master": {"remote": "origin", "merge": "refs/heads/master"},
+            "docker": {"remote": "origin", "merge": "refs/heads/docker"},
+            "python3": {"remote": "origin", "merge": "refs/heads/python3"},
+            "localdev": {"remote": "origin", "merge": "refs/heads/localdev"},
+        },
+        "remote": {
+            "origin": {
+                "url": "git@github.com:me/repo.git",
+                "fetch": "+refs/heads/*:refs/remotes/origin/*",
+            },
+            "upstream": {
+                "url": "git@github.com:upstream/repo.git",
+                "fetch": "+refs/heads/*:refs/remotes/upstream/*",
+            },
+        }
+    }
+    parsed_config = _parse_gitconfig(config)
+    assert parsed_config == expected_result
